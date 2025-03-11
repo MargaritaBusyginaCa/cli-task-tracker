a66e1d7e5b3a2a3940d543ad7b06ffa416e8abc1
Margarita (Rita) Busygina <busygina.rita.ca@gmail.com>
2025-03-11 16:48:35 -0400

    add findTaskIndex funcion and start ids from 1

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-//to-do: add, update, delete, mark-in-progress, mark-done, list
-
 // import the readline module for work with stdin, or stdout.
 const readline = require("readline");
 // import the fs module to write/read files.
 const fs = require("fs");
 
-const prompts = readline.createInterface(process.stdin, process.stdout);
-const tasks = JSON.parse(fs.readFileSync("tasks.json")).tasks;
+const rl = readline.createInterface(process.stdin, process.stdout);
+let tasks = JSON.parse(fs.readFileSync("tasks.json")).tasks;
 
-prompts.question("task-cli: ", (response) => {
-  const args = response.split(",");
-  switch (args[0].toLowerCase()) {
-    case "add":
-      addTask(args);
-      break;
-    case "update":
-      updateTask(args);
-      break;
-    case "delete":
-      break;
-    case "mark-in-progress":
-      markProgress(args, "in-progress");
-      break;
-    case "mark-done":
-      markProgress(args, "done");
-      break;
-    case "list":
-      list();
-      break;
-  }
-  process.exit();
-});
+let recursiveAsyncReadLine = function () {
+  rl.question("task-cli: ", (response) => {
+    const args = response.split(",");
+    if (response == "exit") {
+      return rl.close();
+    } else {
+      switch (args[0].toLowerCase()) {
+        case "add":
+          addTask(args);
+          break;
+        case "update":
+          updateTask(args);
+          break;
+        case "delete":
+          deleteTask(args);
+          break;
+        case "mark-in-progress":
+          markProgress(args, "in-progress");
+          break;
+        case "mark-done":
+          markProgress(args, "done");
+          break;
+        case "list":
+          list();
+          break;
+      }
+      recursiveAsyncReadLine();
+    }
+  });
+};
+recursiveAsyncReadLine();
 
 function addTask(args) {
   const date = new Date();
   const newTask = {
-    id: tasks.length,
+    id: tasks.length + 1,
     description: args[1],
     status: "todo",
     createdAt: date,
@@ -47,35 +53,37 @@ function addTask(args) {
 
 function updateTask(args) {
   const date = new Date();
-  for (let task of tasks) {
-    if (task.id == args[1]) {
-      const updatedTask = {
-        ...task,
-        description: args[2],
-        updatedAt: date,
-      };
-      tasks[task.id] = updatedTask;
-      writeJson();
-    }
-  }
+  const taskIndex = findTaskIndex(args[1]);
+  const updatedTask = {
+    ...tasks[taskIndex],
+    description: args[2],
+    updatedAt: date,
+  };
+  tasks[taskIndex] = updatedTask;
+  writeJson();
 }
 
 function markProgress(args, status) {
   const date = new Date();
-  for (let task of tasks) {
-    if (task.id == args[1]) {
-      const updatedTask = {
-        ...task,
-        status: status,
-        updatedAt: date,
-      };
-      tasks[task.id] = updatedTask;
-      writeJson();
-    }
-  }
+  const taskIndex = findTaskIndex(args[1]);
+  const updatedTask = {
+    ...tasks[taskIndex],
+    status: status,
+    updatedAt: date,
+  };
+  tasks[taskIndex] = updatedTask;
+  writeJson();
 }
 
-function list() {}
+function deleteTask(args) {
+  const taskIndex = findTaskIndex(args[1]);
+  tasks.splice(taskIndex, 1);
+  writeJson();
+}
+
+function list() {
+  console.log(tasks);
+}
 
 function writeJson() {
   const jsonFile = {
@@ -83,4 +91,12 @@ function writeJson() {
   };
   let data = JSON.stringify(jsonFile);
   fs.writeFileSync("tasks.json", data);
+}
+
+function findTaskIndex(taskId) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == taskId) {
+      return i;
+    }
+  }
 }
